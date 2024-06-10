c9170e2a0cb2a57672e3fa9179ea1268c5733d8c
saito-kazumasa <kotobukisyo@gmail.com>
2024-06-10 13:48:10 +0900

    :tada: プロジェクト作成

---
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/EquipmentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.equipmentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jp/ac/morijyobi/equipmentmanagementsystem/EquipmentManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.morijyobi.equipmentmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
