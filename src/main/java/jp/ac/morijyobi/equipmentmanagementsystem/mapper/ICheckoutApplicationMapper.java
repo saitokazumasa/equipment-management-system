e170644ff6b4a0c7ef4a805e9932335afd7089b6
saito-kazumasa <kotobukisyo@gmail.com>
2024-06-12 11:31:23 +0900

    :+1: ICheckoutApplicationMapperのinsert()に@optionを追加

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/ICheckoutApplicationMapper.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/ICheckoutApplicationMapper.java
@@ -3,10 +3,12 @@ package jp.ac.morijyobi.equipmentmanagementsystem.mapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.CheckoutApplication;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface ICheckoutApplicationMapper {
     @Insert("insert into checkout_applications (equipment_id, account_id) " +
             "values (#{equipmentId}, #{accountId})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     public void insert(final CheckoutApplication checkoutApplication);
 }
