a0e83cfdfd17349f61b6b9c8c04285a6d8d9a9c3
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-12 14:36:45 +0900

    mapperを作成

---
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IEquipmentMapper.java
@@ -0,0 +1,15 @@
+package jp.ac.morijyobi.equipmentmanagementsystem.mapper;
+
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+@Mapper
+public interface IEquipmentMapper {
+
+    @Insert("INSERT INTO equipment (name, category_id, storage_location_id, state, lending_period, notification_date, remark) " +
+            "VALUES (#{name}, #{categoryId}, #{storageLocationId}, #{state}, #{lendingPeriod}, #{notificationDate}, #{remark})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    public void insert(Equipment equipment);
+}
