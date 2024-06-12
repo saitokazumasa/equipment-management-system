c52df7f3eb875b21d7c65ed8fe768906dcc3c693
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-12 11:31:51 +0900

    insert文の追記

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IStudentMapper.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IStudentMapper.java
@@ -1,4 +1,15 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.mapper;
 
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Student;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+
+@Mapper
 public interface IStudentMapper {
+
+    @Insert("INSERT INTO students (account_id, course_id, admission_year, graduation_year) " +
+            "VALUES (#{account_id}, #{course_id}, #{admission_year}, #{graduation_year})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    public void insert(final Student student);
 }
