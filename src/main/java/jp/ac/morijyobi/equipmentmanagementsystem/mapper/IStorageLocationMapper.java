9ba318e295dce586e5b6395e7036e43e9a8e0965
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-12 15:08:56 +0900

    insert文を追加

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IStorageLocationMapper.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IStorageLocationMapper.java
@@ -1,8 +1,13 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.mapper;
 
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.StorageLocation;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface IStorageLocationMapper {
-
+    @Insert("INSERT INTO storage_location (name) VALUES (#{name})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(StorageLocation storageLocation);
 }
