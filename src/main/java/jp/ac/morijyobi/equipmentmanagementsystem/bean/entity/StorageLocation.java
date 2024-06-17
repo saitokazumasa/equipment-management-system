3aac619737e00cd45abaa428cc5ba8e3dc1a30e8
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-17 12:22:50 +0900

    Revert "ついでにStorageLocationのentityを修正"
    This reverts commit 5b35c1ac70ddd1e45345078eece7bb74c76dc291.

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/entity/StorageLocation.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/entity/StorageLocation.java
@@ -1,13 +1,22 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.bean.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class StorageLocation {
     private int id;
-    private String name;
+    private final String name;
+
+    public StorageLocation(final int id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setId(final int id) {
+        this.id = id;
+    }
 }
