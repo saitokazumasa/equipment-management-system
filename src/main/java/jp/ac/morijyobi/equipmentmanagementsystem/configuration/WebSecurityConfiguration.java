bd5fb2974f924c36c6bc1c9b3cf16daf408880cd
saito-kazumasa <kotobukisyo@gmail.com>
2024-06-17 12:23:57 +0900

    :+1: WebSecurityConfig を作成

---
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,20 @@
+package jp.ac.morijyobi.equipmentmanagementsystem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(final HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated()
+                ).build();
+    }
+}
