5f0f9de562dcf248bf795156fb0c563eb42653c8
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-17 15:15:24 +0900

    CheckoutControllerを作成

---
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/CheckoutController.java
@@ -0,0 +1,15 @@
+package jp.ac.morijyobi.equipmentmanagementsystem.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/checkout")
+public class CheckoutController {
+    @GetMapping("/application")
+    public String application() {
+        return "checkout/application";
+    }
+}
