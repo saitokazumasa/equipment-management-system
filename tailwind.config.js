bdd559b689eeb25ab1d6e309a8a6dd4f0dfba431
saito-kazumasa <kotobukisyo@gmail.com>
2024-06-19 16:26:07 +0900

    :+1: ログイン画面 main 内のコンポーネントを作成

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#0078C8",
+        "secondary": "#A8DCFF",
+        "error": "#C82400",
+        "secondary-error": "#FFB5A5",
+        "gray": "#323232",
+        "content-text": "#050505",
+        "background": "#F2FAFF",
+      },
+    },
   },
   plugins: [],
 }
