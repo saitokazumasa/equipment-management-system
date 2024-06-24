ef146a18ab58b495a12a8138b94cd1a365769195
saito-kazumasa <kotobukisyo@gmail.com>
2024-06-24 14:25:28 +0900

    :fire: テスト用の不要な文を削除

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/LoginController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/LoginController.java
@@ -1,7 +1,6 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class LoginController {
 
     @GetMapping()
-    public String get(Model model) {
+    public String get() {
         return "login";
     }
 }
