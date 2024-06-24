f093e2c2e0bc5dd7d1aed5af1016b66e186d72a4
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-24 13:44:49 +0900

    エラーメッセージを表示するように修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/CheckoutController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/CheckoutController.java
@@ -5,10 +5,11 @@ import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.CheckoutApplicationsService;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.EquipmentsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/checkout")
@@ -22,18 +23,34 @@ public class CheckoutController {
     }
 
     @GetMapping("/application")
-    public String application() {
+    public String application(Model model) {
+        model.addAttribute("message", "備品が選択されていません。");
         return "checkout/application";
     }
 
     @PostMapping("/application")
-    public String application(@RequestParam String[] equipmentIds) {
-        for (String equipmentId : equipmentIds) {
-            int id = Integer.parseInt(equipmentId);
-            CheckoutApplication checkoutApplication = new CheckoutApplication(-1, id, 1, LocalDateTime.now());
-            checkoutApplicationsService.insert(checkoutApplication);
+    public String application(@RequestParam String[] equipmentIds, RedirectAttributes redirectAttributes) {
+        // 備品が選択されていない場合は申請画面に戻る
+        if (equipmentIds.length == 0) {
+            redirectAttributes.addFlashAttribute("message", "備品が選択されていません");
+            return "redirect:/checkout/application";
         }
-        return "redirect:/checkout/application-result";
+
+        try {
+            for (String equipmentId : equipmentIds) {
+                int id = Integer.parseInt(equipmentId);
+                CheckoutApplication checkoutApplication = new CheckoutApplication(-1, id, 1, LocalDateTime.now());
+                checkoutApplicationsService.insert(checkoutApplication);
+            }
+            redirectAttributes.addFlashAttribute("result", "送信しました。");
+            redirectAttributes.addFlashAttribute("message", "備品を持ち、管理者に承認をお願いしてください。");
+            return "redirect:/checkout/application-result";
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("result", "送信時にエラーが発生しました");
+            redirectAttributes.addFlashAttribute("message", "時間をおいて再度 送信してください。\n" +
+                    "何度もエラーが出る場合はサポートにお問い合わせください。");
+            return "redirect:/checkout/application-result";
+        }
     }
 
     // 対象の備品の情報を返す
