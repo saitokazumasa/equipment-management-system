252c9785becee5c4d73a51b7db41465980daa522
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-26 13:33:14 +0900

    コメントアウトを解除しました。

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IEditLogsMapper.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/IEditLogsMapper.java
@@ -5,11 +5,11 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 
-//@Mapper
-//public interface IEditLogsMapper {
-//
-//    @Insert("INSERT INTO edit_logs (equipment_id, account_id, reason, before_equipment_data, after_equipment_data) " +
-//            "VALUES (#{equipmentId}, #{accountId}, #{reason}, #{beforeEquipmentData}, #{afterEquipmentData})")
-//    @Options(useGeneratedKeys = true, keyProperty = "id")
-//    public void insert(final EditLog editLog);
-//}
+@Mapper
+public interface IEditLogsMapper {
+
+    @Insert("INSERT INTO edit_logs (equipment_id, account_id, reason, before_equipment_data, after_equipment_data) " +
+            "VALUES (#{equipmentId}, #{accountId}, #{reason}, #{beforeEquipmentData}, #{afterEquipmentData})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    public void insert(final EditLog editLog);
+}
