6d2477ee5bec52900da641e5841d15ae3331b34a
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-06-26 11:51:08 +0900

    アノテーションを削除

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/CheckoutApplicationsService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/CheckoutApplicationsService.java
@@ -2,11 +2,6 @@ package jp.ac.morijyobi.equipmentmanagementsystem.service;
 
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.CheckoutApplication;
 
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
 public interface CheckoutApplicationsService {
 
     public void insert(CheckoutApplication checkoutApplication);
--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/EquipmentsService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/EquipmentsService.java
@@ -1,9 +1,7 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.service;
 
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface EquipmentsService {
     void insert(Equipment equipment);
 
