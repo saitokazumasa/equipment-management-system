c5abfd90230402e84fe536177e1137df89c89b82
saito-kazumasa <kotobukisyo@gmail.com>
2024-07-04 13:11:43 +0900

    :+1: メニューを作成

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,8 +1,14 @@
-const accountIconElement = document.getElementById('account-icon');
-const accountMenuElement = document.getElementById('account-menu');
+const menuElement = document.getElementById('menu');
+const menuOpenButtonElement = document.getElementById('menu-open-button');
+const menuCloseButtonElement = document.getElementById('menu-close-button');
 
-function onClickAccountIcon(event) {
-    accountMenuElement.hidden = !accountMenuElement.hidden;
+function onMenuOpenButtonClick(event) {
+    menuElement.hidden = false;
 }
 
-accountIconElement?.addEventListener('click', onClickAccountIcon);
+function onMenuCloseButtonClick(event) {
+    menuElement.hidden = true;
+}
+
+menuOpenButtonElement?.addEventListener('click', onMenuOpenButtonClick);
+menuCloseButtonElement?.addEventListener('click', onMenuCloseButtonClick);
