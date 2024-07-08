ef57807ade906365c747b575f7e6d970248dd08a
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-07-08 15:11:29 +0900

    entity作成

---
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/entity/JsonReturnEquipment.java
@@ -0,0 +1,25 @@
+package jp.ac.morijyobi.equipmentmanagementsystem.bean.entity;
+
+import jp.ac.morijyobi.equipmentmanagementsystem.constant.EquipmentState;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JsonReturnEquipment {
+    private int id;
+    private String name;
+    private int categoryId;
+    private int storageLocationId;
+    private EquipmentState state;
+    private int lendingPeriod;
+    private int notificationDate;
+    private String remark;
+    private LocalDateTime createdAt;
+    private String damage;
+    private String damageReason;
+}
