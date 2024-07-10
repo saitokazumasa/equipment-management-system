2604b0d0ed67c94d1d2e3b7264959f3c973b408d
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-07-10 13:49:03 +0900

    formクラスを修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/ReturnApplicationForm.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/ReturnApplicationForm.java
@@ -5,32 +5,40 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Damage;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
+
+import java.util.List;
 
 public class ReturnApplicationForm {
     @NotBlank
     @Size(min = 1)
     private final String json;
 
+    private final String damageList;
+
     @NotBlank
     private final String mail;
 
     private final ObjectMapper objectMapper;
 
-    public ReturnApplicationForm(final String json, final String mail) {
+    public ReturnApplicationForm(final String json, String damageList, final String mail) {
         this.json = json;
+        this.damageList = damageList;
         this.mail = mail;
         objectMapper = new ObjectMapper()
                 .registerModule(new JavaTimeModule());
     }
 
     public static ReturnApplicationForm generate(final String mail) {
-        return new ReturnApplicationForm("", mail);
+        return new ReturnApplicationForm("", "", mail);
     }
 
     public String json() {
         return json;
     }
+
+    public String damageList() { return damageList; }
 
     public String mail() {
         return mail;
@@ -47,6 +55,22 @@ public class ReturnApplicationForm {
     private Equipment[] tryToArray(final String json) {
         try {
             return objectMapper.readValue(json, Equipment[].class);
+        } catch (final JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Damage[] damages() {
+        final Damage[] damages = tryToDamageArray(damageList);
+
+        if (damages.length == 0) throw new ArrayIndexOutOfBoundsException();
+
+        return damages;
+    }
+
+    private Damage[] tryToDamageArray(final String json) {
+        try {
+            return objectMapper.readValue(json, Damage[].class);
         } catch (final JsonProcessingException e) {
             throw new RuntimeException(e);
         }
