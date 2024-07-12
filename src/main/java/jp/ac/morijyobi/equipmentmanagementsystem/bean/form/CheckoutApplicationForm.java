2c6ca3c6aa7b6e7289722ee57cabab61e02d6ffa
saito-kazumasa <kotobukisyo@gmail.com>
2024-07-12 15:21:24 +0900

    :recycle: CheckoutApplicationForm から mail を削除& Json Util を使う方式に変更

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/CheckoutApplicationForm.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/CheckoutApplicationForm.java
@@ -1,56 +1,33 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.bean.form;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
-
-import java.util.EmptyStackException;
+import jp.ac.morijyobi.equipmentmanagementsystem.util.JsonUtil;
+import lombok.Getter;
 
 public class CheckoutApplicationForm {
+    @Getter
     @NotBlank
     @Size(min = 1)
     private final String json;
 
-    @NotBlank
-    private final String mail;
+    private final JsonUtil<Equipment[]> jsonUtil;
 
-    private final ObjectMapper objectMapper;
-
-    public CheckoutApplicationForm(final String json, final String mail) {
+    public CheckoutApplicationForm(final String json) {
         this.json = json;
-        this.mail = mail;
-        objectMapper = new ObjectMapper()
-                .registerModule(new JavaTimeModule());
+        this.jsonUtil = new JsonUtil<>(Equipment[].class);
     }
 
-    public static CheckoutApplicationForm generate(final String mail) {
-        return new CheckoutApplicationForm("", mail);
-    }
-
-    public String json() {
-        return json;
-    }
-
-    public String mail() {
-        return mail;
+    public static CheckoutApplicationForm empty() {
+        return new CheckoutApplicationForm("");
     }
 
     public Equipment[] equipments() {
-        final Equipment[] equipments = tryToArray(json);
+        final Equipment[] equipments = jsonUtil.tryToObject(json);
 
         if (equipments.length == 0) throw new ArrayIndexOutOfBoundsException();
 
         return equipments;
-    }
-
-    private Equipment[] tryToArray(final String json) {
-        try {
-            return objectMapper.readValue(json, Equipment[].class);
-        } catch (final JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
