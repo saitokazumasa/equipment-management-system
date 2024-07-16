b5d6e350444678e561168cc77a0fc4ba644e9e15
AirikU <a.usami.sys21@morijyobi.ac.jp>
2024-07-16 12:16:34 +0900

    [update]returnのパスを少し簡略化

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PasswordChangeController {
     @GetMapping()
     public String get() {
-        return "/password/change/password_change";
+        return "password/change/password_change";
     }
 }
