a5ec02b7c6fd32937d27701a66674e2d9bf8976a
AirikU <a.usami.sys21@morijyobi.ac.jp>
2024-07-17 14:31:45 +0900

    [update]PostMapping()の処理をメッセージを表示・redirectする処理に変更

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
@@ -1,26 +1,33 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.controller;
 
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.output.ErrorMessage;
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.output.SuccessMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
-@RequestMapping("/password/change/**")
+@RequestMapping("/password_change")
 public class PasswordChangeController {
     @GetMapping()
     public String get() {
-        return "password/change/password_change";
+        return "password_change";
     }
 
     @PostMapping()
-    public String post() {
+    public String post(final RedirectAttributes redirectAttributes) {
         final boolean result = true;
 
         if (result) {
-            return "password/change/success";
+            final var message = new SuccessMessage("送信しました。");
+            redirectAttributes.addFlashAttribute("message", message);
         } else {
-            return "password/change/failed";
+            final var message = new ErrorMessage("送信時にエラーが発生しました。");
+            redirectAttributes.addFlashAttribute("message", message);
         }
+
+        return "redirect:/password_change";
     }
 }
