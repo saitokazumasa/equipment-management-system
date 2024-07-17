b53dbc84507e80dace85882614e5a5faf804ea10
AirikU <a.usami.sys21@morijyobi.ac.jp>
2024-07-17 11:53:18 +0900

    [add]送信結果画面(成功・失敗)に遷移する処理を追加

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/PasswordChangeController.java
@@ -2,13 +2,25 @@ package jp.ac.morijyobi.equipmentmanagementsystem.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/password/change")
+@RequestMapping("/password/change/**")
 public class PasswordChangeController {
     @GetMapping()
     public String get() {
         return "password/change/password_change";
+    }
+
+    @PostMapping()
+    public String post() {
+        final boolean result = false;
+
+        if (result) {
+            return "password/change/success";
+        } else {
+            return "password/change/failed";
+        }
     }
 }
