ac5016bb3b10b5935d340760e387c4c20866fc8b
saito-kazumasa <kotobukisyo@gmail.com>
2024-07-19 11:59:17 +0900

    :recycle: JsonUtil を static 化

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/util/JsonUtil.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/util/JsonUtil.java
@@ -4,30 +4,27 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
-public class JsonUtil<T> {
-    private final ObjectMapper objectMapper;
-    private final Class<T> javaType;
-
-    public JsonUtil(final Class<T> javaType) {
-        this.javaType = javaType;
-
-        this.objectMapper = new ObjectMapper();
-        this.objectMapper.registerModule(new JavaTimeModule());
+public class JsonUtil {
+    private static final ObjectMapper objectMapper =
+            new ObjectMapper()
+                    .registerModule(new JavaTimeModule());
+    
+    private JsonUtil() {
     }
 
-    public T tryToObject(final Object json) {
+    public static <T> T tryToObject(final Object json, final Class<T> javaType) {
         if (json == null) return null;
 
         try {
-            return this.objectMapper.readValue(json.toString(), javaType);
+            return objectMapper.readValue(json.toString(), javaType);
         } catch (final JsonProcessingException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public String tryToString(final T object) {
+    public static <T> String tryToString(final T object) {
         try {
-            return this.objectMapper.writeValueAsString(object);
+            return objectMapper.writeValueAsString(object);
         } catch (final JsonProcessingException e) {
             throw new RuntimeException(e);
         }
