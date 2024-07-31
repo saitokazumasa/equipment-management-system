e2db25bb50fc2636b8cbc9164f4d858ac9775738
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-07-31 17:35:32 +0900

    ServiceとMapperを作成

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipment/implement/RegisterEquipmentService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipment/implement/RegisterEquipmentService.java
@@ -1,6 +1,8 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.service.equipment.implement;
 
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.form.equipmentRegisterForm;
+import jp.ac.morijyobi.equipmentmanagementsystem.constant.EquipmentState;
 import jp.ac.morijyobi.equipmentmanagementsystem.mapper.IEquipmentsMapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.equipment.IRegisterEquipmentService;
 import org.springframework.stereotype.Service;
@@ -13,8 +15,26 @@ public class RegisterEquipmentService implements IRegisterEquipmentService {
         this.equipmentsMapper = equipmentsMapper;
     }
 
+
     @Override
-    public int execute(final Equipment equipment) {
+    public int execute(equipmentRegisterForm equipmentRegisterForm) {
+        final Equipment equipment = new Equipment();
+
+        equipment.setName(equipmentRegisterForm.getEquipmentName());
+        equipment.setCategoryId(Integer.parseInt(equipmentRegisterForm.getCategory()));
+        equipment.setStorageLocationId(1);
+        equipment.setState(EquipmentState.AVAILABLE_FOR_LOAN);
+
+        if (equipmentRegisterForm.getIsPeriod()) {
+            equipment.setLendingPeriod(equipmentRegisterForm.getLendingPeriod());
+            equipment.setNotificationDate(equipmentRegisterForm.getNotificationDate());
+        } else {
+            equipment.setLendingPeriod(-1);
+            equipment.setNotificationDate(-1);
+        }
+
+        equipment.setRemark(equipmentRegisterForm.getRemark());
+
         return this.equipmentsMapper.insert(equipment);
     }
 }
