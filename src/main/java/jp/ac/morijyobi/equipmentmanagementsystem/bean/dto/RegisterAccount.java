7c52edf61ad6aa64922ae453a559f6799d7bbe77
saito-kazumasa <kotobukisyo@gmail.com>
2024-08-15 15:59:03 +0900

    :+1: アカウント登録機能のページ遷移などを作成

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/dto/RegisterAccount.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/dto/RegisterAccount.java
@@ -21,4 +21,8 @@ public class RegisterAccount implements Serializable{
 
     @NotBlank
     private String category;
+
+    public static RegisterAccount empty() {
+        return new RegisterAccount();
+    }
 }
--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/RegisterAccountController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/RegisterAccountController.java
@@ -1,5 +1,7 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.dto.RegisterAccount;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.dto.RegisterAccountList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,35 +10,104 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/account/registration")
 public class RegisterAccountController {
     @GetMapping()
     public String get(final Model model) {
-        final var registerAccountList = RegisterAccountList.empty();
-        model.addAttribute(registerAccountList);
+        // リダイレクトしてきた場合は registerAccount のみ初期化
+        final boolean isRedirect = model.containsAttribute("registerAccountList");
+        if (isRedirect) {
+            final RegisterAccount registerAccount = RegisterAccount.empty();
+            model.addAttribute("registerAccount", registerAccount);
+            return "account/registration/registration";
+        }
+
+        final RegisterAccount registerAccount = RegisterAccount.empty();
+        final RegisterAccountList registerAccountList = RegisterAccountList.empty();
+
+        model.addAttribute("registerAccount", registerAccount);
+        model.addAttribute("registerAccountList", registerAccountList);
+
         return "account/registration/registration";
     }
 
-    @PostMapping()
-    public String post(
+    @PostMapping(params = "add")
+    public String add(
+            final @Validated RegisterAccount registerAccount,
+            final BindingResult registerAccountBindingResult,
+            final @Validated RegisterAccountList registerAccountList,
+            // NOTE: これがないと初期状態の空リストを通せない
+            final BindingResult __,
+            final RedirectAttributes redirectAttributes
+    ) {
+        if (registerAccountBindingResult.hasErrors()) return "account/registration/registration";
+
+        final RegisterAccountList newRegisterAccountList = registerAccountList.isEmpty() ?
+                RegisterAccountList.empty().add(registerAccount) :
+                registerAccountList.add(registerAccount);
+
+        redirectAttributes.addFlashAttribute("registerAccountList", newRegisterAccountList);
+
+        return "redirect:/account/registration";
+    }
+
+    @PostMapping(params = "remove")
+    public String remove(
+            final @Validated RegisterAccount registerAccount,
+            final BindingResult _1,
+            final @Validated RegisterAccountList registerAccountList,
+            final BindingResult _2,
+            final HttpServletRequest request,
+            final RedirectAttributes redirectAttributes
+    ) {
+        final int index = Integer.parseInt(request.getParameter("remove"));
+        final RegisterAccountList newRegisterAccountList = registerAccountList.remove(index);
+
+        redirectAttributes.addFlashAttribute("registerAccountList", newRegisterAccountList);
+
+        return "redirect:/account/registration";
+    }
+
+    @PostMapping(params = "confirm")
+    public String confirm(
+            final @Validated RegisterAccount registerAccount,
+            final BindingResult __,
             final @Validated RegisterAccountList registerAccountList,
             final BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) return "account/registration/registration";
 
-        return "account/registration/confirm";
+        return "account/registration/confirmation";
     }
 
     @PostMapping(params = "submit")
     public String submit(
+            final @Validated RegisterAccount registerAccount,
+            final BindingResult __,
             final @Validated RegisterAccountList registerAccountList,
             final BindingResult bindingResult
     ) {
         if (bindingResult.hasErrors()) return "account/registration/registration";
 
-        // TODO: 結果画面へ
-        return "account/registration/confirm";
+        // TODO: 登録し、結果を取得する
+
+        final boolean result = true;
+
+        if (result) return "redirect:/account/registration/success";
+
+        return "redirect:/account/registration/failed";
+    }
+
+    @GetMapping("success")
+    public String success() {
+        return "account/registration/success";
+    }
+
+    @GetMapping("failed")
+    public String failed() {
+        return "account/registration/failed";
     }
 }
