f9f5bc6b7dad7e2b8d63ccfc2d4aad628c24a115
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-08-15 21:36:25 +0900

    備品を登録する際のカテゴリに関する処理を修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipment/implement/RegisterEquipmentService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipment/implement/RegisterEquipmentService.java
@@ -1,8 +1,10 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.service.equipment.implement;
 
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Equipment;
+import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.EquipmentCategory;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.form.equipmentRegisterForm;
 import jp.ac.morijyobi.equipmentmanagementsystem.constant.EquipmentState;
+import jp.ac.morijyobi.equipmentmanagementsystem.mapper.IEquipmentCategoriesMapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.mapper.IEquipmentsMapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.equipment.IRegisterEquipmentService;
 import org.springframework.stereotype.Service;
@@ -10,18 +12,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class RegisterEquipmentService implements IRegisterEquipmentService {
     private final IEquipmentsMapper equipmentsMapper;
+    private final IEquipmentCategoriesMapper equipmentCategoriesMapper;
 
-    public RegisterEquipmentService(final IEquipmentsMapper equipmentsMapper) {
+    public RegisterEquipmentService(final IEquipmentsMapper equipmentsMapper, IEquipmentCategoriesMapper equipmentCategoriesMapper) {
         this.equipmentsMapper = equipmentsMapper;
+        this.equipmentCategoriesMapper = equipmentCategoriesMapper;
     }
 
 
     @Override
     public int execute(equipmentRegisterForm equipmentRegisterForm) {
         final Equipment equipment = new Equipment();
+        final EquipmentCategory equipmentCategory = new EquipmentCategory();
+
+
+        final EquipmentCategory a = equipmentCategoriesMapper.selectByName(equipmentRegisterForm.getCategory());
+
+        // aの結果がnullの場合、新規登録
+        if (a == null) {
+            equipmentCategory.setName(equipmentRegisterForm.getCategory());
+            equipmentCategoriesMapper.insert(equipmentCategory);
+            equipment.setCategoryId(equipmentCategoriesMapper.selectByName(equipmentRegisterForm.getCategory()).getId());
+        } else {
+            equipment.setCategoryId(a.getId());
+        }
 
         equipment.setName(equipmentRegisterForm.getEquipmentName());
-        equipment.setCategoryId(Integer.parseInt(equipmentRegisterForm.getCategory()));
         equipment.setStorageLocationId(1);
         equipment.setState(EquipmentState.AVAILABLE_FOR_LOAN);
 
