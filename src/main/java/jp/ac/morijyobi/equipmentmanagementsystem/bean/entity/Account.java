c4a070166ede865c564dca8a7373429b8ecb4e36
saito-kazumasa <kotobukisyo@gmail.com>
2024-08-17 22:27:30 +0900

    :recycle: PasswordEncoder の使い方をリファクタ

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/entity/Account.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/entity/Account.java
@@ -4,8 +4,6 @@ import jp.ac.morijyobi.equipmentmanagementsystem.constant.AccountCategory;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Data
 @AllArgsConstructor
@@ -17,18 +15,4 @@ public class Account {
     public String password;
     public AccountCategory category;
     public Boolean isEnable;
-
-    public Account cryptPassword() {
-        final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        final String newPassword = passwordEncoder.encode(this.password);
-
-        return new Account(
-                this.id,
-                this.name,
-                this.mail,
-                newPassword,
-                this.category,
-                this.isEnable
-        );
-    }
 }
--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/account/implement/RegisterAccountService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/account/implement/RegisterAccountService.java
@@ -5,15 +5,21 @@ import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.Account;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.dto.RegisterAccount;
 import jp.ac.morijyobi.equipmentmanagementsystem.mapper.IAccountsMapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.account.IRegisterAccountService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class RegisterAccountService implements IRegisterAccountService {
     private final IAccountsMapper accountsMapper;
+    private final PasswordEncoder passwordEncoder;
 
-    public RegisterAccountService(final IAccountsMapper accountsMapper) {
+    public RegisterAccountService(
+            final IAccountsMapper accountsMapper,
+            final PasswordEncoder passwordEncoder
+    ) {
         this.accountsMapper = accountsMapper;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -21,11 +27,24 @@ public class RegisterAccountService implements IRegisterAccountService {
     public boolean execute(final RegisterAccountList registerAccountList) {
         for (final RegisterAccount registerAccount : registerAccountList.getValues()) {
             final Account account = registerAccount.toAccount();
-            final Account newAccount = account.cryptPassword();
+            final Account newAccount = cryptPassword(account);
             final int result = this.accountsMapper.insert(newAccount);
 
             if (result != 1) return false;
         }
         return true;
+    }
+
+    private Account cryptPassword(final Account account) {
+        final String password = passwordEncoder.encode(account.getPassword());
+
+        return new Account(
+                account.id,
+                account.name,
+                account.mail,
+                password,
+                account.category,
+                account.isEnable
+        );
     }
 }
