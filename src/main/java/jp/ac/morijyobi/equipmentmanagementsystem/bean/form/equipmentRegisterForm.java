c8124679aa43474af0585a4b677afbff5b5757ce
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-08-17 16:16:25 +0900

    @getter, @setterを使用するように変更

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/equipmentRegisterForm.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/equipmentRegisterForm.java
@@ -4,7 +4,11 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class equipmentRegisterForm {
     @NotBlank
     @Size(min = 1, max = 256)
@@ -30,54 +34,5 @@ public class equipmentRegisterForm {
     public equipmentRegisterForm() {
         this.lendingPeriod = 7;
         this.notificationDate = 3;
-    }
-
-
-    public @NotBlank @Size(min = 1, max = 256) String getEquipmentName() {
-        return equipmentName;
-    }
-
-    public void setEquipmentName(@NotBlank @Size(min = 1, max = 256) String equipmentName) {
-        this.equipmentName = equipmentName;
-    }
-
-    public @NotBlank String getCategory() { return category; }
-
-    public void setCategory(@NotBlank String category) { this.category = category; }
-
-    public @NotNull Boolean getIsPeriod() {
-        return isPeriod;
-    }
-
-    public void setIsPeriod(@NotNull Boolean period) {
-        isPeriod = period;
-    }
-
-    @NotNull
-    @Min(1)
-    public int getLendingPeriod() {
-        return lendingPeriod;
-    }
-
-    public void setLendingPeriod(@NotNull @Min(1) int lendingPeriod) {
-        this.lendingPeriod = lendingPeriod;
-    }
-
-    @NotNull
-    @Min(1)
-    public int getNotificationDate() {
-        return notificationDate;
-    }
-
-    public void setNotificationDate(@NotNull @Min(1) int notificationDate) {
-        this.notificationDate = notificationDate;
-    }
-
-    public @Size(max = 512) String getRemark() {
-        return remark;
-    }
-
-    public void setRemark(@Size(max = 512) String remark) {
-        this.remark = remark;
     }
 }
