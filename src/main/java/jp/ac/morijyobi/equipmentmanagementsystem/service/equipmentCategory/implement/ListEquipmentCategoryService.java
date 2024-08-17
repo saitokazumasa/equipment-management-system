9aec8603bcb3c0befca89b4b0e660eb9f0356c80
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-08-17 17:27:47 +0900

    serviceつけ忘れていたので修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipmentCategory/implement/ListEquipmentCategoryService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/equipmentCategory/implement/ListEquipmentCategoryService.java
@@ -4,9 +4,11 @@ package jp.ac.morijyobi.equipmentmanagementsystem.service.equipmentCategory.impl
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.EquipmentCategory;
 import jp.ac.morijyobi.equipmentmanagementsystem.mapper.IEquipmentCategoriesMapper;
 import jp.ac.morijyobi.equipmentmanagementsystem.service.equipmentCategory.IListEquipmentCategoryService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ListEquipmentCategoryService implements IListEquipmentCategoryService {
     private final IEquipmentCategoriesMapper equipmentCategoriesMapper;
 
