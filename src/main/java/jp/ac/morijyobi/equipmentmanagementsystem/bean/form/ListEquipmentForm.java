0989b95e935e027b8e1230eec6a6082ea16132b1
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-08-18 15:18:27 +0900

    formを修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/ListEquipmentForm.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/bean/form/ListEquipmentForm.java
@@ -1,6 +1,7 @@
 package jp.ac.morijyobi.equipmentmanagementsystem.bean.form;
 
 import jakarta.validation.constraints.Size;
+import jp.ac.morijyobi.equipmentmanagementsystem.constant.EquipmentState;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
@@ -13,5 +14,5 @@ public class ListEquipmentForm {
 
     private List<Integer> categoryIdList;
 
-    private List<Integer> statusIdList;
+    private List<EquipmentState> stateList;
 }
