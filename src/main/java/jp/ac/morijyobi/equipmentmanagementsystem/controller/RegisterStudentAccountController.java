a2aab4203ab290b9234415ba1d1494887ac8a660
saito-kazumasa <kotobukisyo@gmail.com>
2024-08-19 14:30:20 +0900

    :fire: 不要な行を削除

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/RegisterStudentAccountController.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/controller/RegisterStudentAccountController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.sql.SQLException;
-
 @Controller
 @RequestMapping("/student/registration")
 public class RegisterStudentAccountController {
