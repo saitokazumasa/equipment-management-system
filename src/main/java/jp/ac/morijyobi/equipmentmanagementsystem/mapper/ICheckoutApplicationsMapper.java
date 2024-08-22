e1e331aa5c8726c2128d283a7514d89a1516cf09
ken-250 <k.kawaguchi.sys21@morijyobi.ac.jp>
2024-08-22 10:41:06 +0900

    mapper修正

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/ICheckoutApplicationsMapper.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/mapper/ICheckoutApplicationsMapper.java
@@ -4,6 +4,7 @@ import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.CheckoutApplication
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface ICheckoutApplicationsMapper {
@@ -11,4 +12,15 @@ public interface ICheckoutApplicationsMapper {
             "VALUES (#{equipmentId}, #{accountId})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     public int insert(final CheckoutApplication checkoutApplication);
+
+    @Select("SELECT * FROM checkout_applications c_apply " +
+            "LEFT OUTER JOIN checkout_approvals c_approve ON c_apply.id = c_approve.checkout_application_id " +
+            "LEFT OUTER JOIN return_applications r_apply ON c_apply.id = r_apply.checkout_log_id " +
+            "LEFT OUTER JOIN return_approvals r_approve ON r_apply.id = r_approve.return_application_id " +
+            "WHERE c_approve.checkout_application_id IS NOT NULL " +
+            "AND r_approve.return_application_id IS NULL " +
+            "AND c_apply.equipment_id = #{equipmentId} " +
+            "AND c_apply.account_id = #{accountId} " +
+            "ORDER BY r_approve.created_at DESC LIMIT 1")
+    public CheckoutApplication selectByEquipmentIdAndAccountId(final int equipmentId, final int accountId);
 }
