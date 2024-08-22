ae93d6c7f4318ea900d8baf90b08446619fb11be
ken-250 <101164631+ken-250@users.noreply.github.com>
2024-08-22 11:19:23 +0900

    Update IApplyDamageService.java

--- src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/damage/IApplyDamageService.java
+++ src/main/java/jp/ac/morijyobi/equipmentmanagementsystem/service/damage/IApplyDamageService.java
@@ -3,5 +3,5 @@ package jp.ac.morijyobi.equipmentmanagementsystem.service.damage;
 import jp.ac.morijyobi.equipmentmanagementsystem.bean.entity.DamagedApplication;
 
 public interface IApplyDamageService {
-    public int execute(final DamagedApplication DamagedApplication);
+    public void execute(final DamagedApplication DamagedApplication);
 }
